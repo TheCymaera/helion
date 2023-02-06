9f59d17aa31167f14253690b82a7a80980ce9317
TheCymaera <69355340+TheCymaera@users.noreply.github.com>
2023-02-07 06:44:06 +0800

    ✨ AltSurface & CodeBlock dedent

--- src/containers/CodeBlock.ts
+++ src/containers/CodeBlock.ts
@@ -1,1 +1,33 @@
 import "./CodeBlock.css";
+
+export class CodeBlockElement extends HTMLElement {
+	connectedCallback() {
+		if (this.hasAttribute("dedent")) {
+			this.textContent = dedent(this.textContent!);
+			this.removeAttribute("dedent");
+		}
+	}
+}
+customElements.define("helion-code-block", CodeBlockElement);
+
+
+function dedent(text: string) {
+	const lines = text.split("\n");
+
+	// remove leading and trailing line breaks
+	while (!lines[0]?.trim()) lines.shift();
+	while (!lines[lines.length - 1]?.trim()) lines.pop();
+
+	let minIndent = Infinity;
+	for (const line of lines) {
+		if (line.trim().length === 0) continue;
+		const indent = line.match(/^\s*/)![0]!.length;
+		if (indent < minIndent) minIndent = indent;
+	}
+
+	for (let i = 0; i < lines.length; i++) {
+		lines[i] = lines[i]!.slice(minIndent);
+	}
+
+	return lines.join("\n");
+}
